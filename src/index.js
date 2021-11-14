3ff3c0acd8987adef6ee25da264ef3219fa6d08f
Hungry Man <katsu.matsushita@gmail.com>
2021-11-14 12:24:13 -0500

    Initial Commit The skeleton created using code used in class activities. package.json copied from what was used in class to develop the React app. Copied in a basic index.html, index.js, app.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// code copied from class activities, and is boilerplate code
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
