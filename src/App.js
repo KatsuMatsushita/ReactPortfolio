de7ec7557e2b21d6c5c75bf2acf06f7b4f91ad88
Hungry Man <katsu.matsushita@gmail.com>
2021-11-26 16:55:11 -0500

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-// boilerplate code copied from class activities with comments
-import React  from 'react';
-import PageContainer from "./components/PageContainer";
-// importing react-bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const App = () => <PageContainer />;
-
-export default App;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-// code copied from class activities, and is boilerplate code
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
